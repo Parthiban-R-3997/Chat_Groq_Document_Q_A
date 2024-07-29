bb59eeda2241861d6d27bc6434d6d46ad4791835
Parthiban-R-3997 <rparthiban729@gmail.com>
2024-07-29 15:54:26 +0100

    Message

--- app.py
+++ app.py
@@ -67,10 +67,12 @@ def vector_embedding(pdf_files):
 
 # Define model options
 model_options = [
+    "llama-3.1-70b-versatile",
+    "llama-3.1-8b-instant",
     "llama3-8b-8192",
     "llama3-70b-8192",
     "mixtral-8x7b-32768",
-    "gemma-7b-it"
+    "gemma2-9b-it"
 ]
 
 # Sidebar elements
